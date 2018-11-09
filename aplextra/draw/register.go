3f2c430a31a734aef5125c2b65f9fc2c38d77df1
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2018-11-09 19:24:39 +0100

    aplextra/draw: new package

---
+++ aplextra/draw/register.go
@@ -0,0 +1,44 @@
+package draw
+
+import (
+	"fmt"
+
+	"github.com/ktye/iv/apl"
+)
+
+func Register(a *apl.Apl) error {
+	must(a.Assign("canvas", Canvas{})) // canvas 0 returns new context
+	must(a.Assign("p", Path{}))        // p A or p A B or p A B C (linear, quad or qubic element)
+	//must(a.Assign("l", function(polyline)))  // l A B C D E ←→ p A p B p C p D p E
+	must(a.Assign("c", function(closepath)))  // c p A p B p C (close path)
+	must(a.Assign("f", function(fillpath)))   // f c p A p B p C (fill path)
+	must(a.Assign("s", function(strokepath))) // s c p A p B p C (stroke path)
+	a.RegisterPrimitive("+", handle(translate))
+	// TODO ctx x Z: multiply by complex Z (scale and rotate)
+	// TODO monadic ⍒ and ⍋for push/pop?
+	// TODO color, linewidth, linedash, text
+
+	return nil
+}
+
+type handle func(*apl.Apl, apl.Value, apl.Value) (bool, apl.Value, error)
+
+func (h handle) HandlePrimitive(a *apl.Apl, l apl.Value, r apl.Value) (bool, apl.Value, error) {
+	return h(a, l, r)
+}
+
+type function func(*apl.Apl, apl.Value, apl.Value) (apl.Value, error)
+
+func (f function) Call(a *apl.Apl, l, r apl.Value) (apl.Value, error) {
+	return f(a, l, r)
+}
+
+func (f function) String(a *apl.Apl) string {
+	return fmt.Sprintf("%T") // TODO: what does this return for s and c?
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
