44a7857005f529710740f081a10e00ac248d4edd
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2018-12-22 23:39:08 +0100

    xgo: functions for interfacing go
    xgo provides converters from apl.Value to reflect.Value
    and back.
    
    It adds a Function type for calling general go functions.
    xgo can be used for building packages, e.g. strings.

---
+++ apl/xgo/convert.go
@@ -0,0 +1,135 @@
+package xgo
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/ktye/iv/apl"
+	"github.com/ktye/iv/apl/numbers"
+)
+
+// Exporter can be implemented by an apl.Value to be able convert it to a go value.
+type Exporter interface {
+	Export() reflect.Value
+}
+
+// export converts an apl value to a go value.
+func export(a *apl.Apl, v apl.Value, t reflect.Type) (reflect.Value, error) {
+
+	if e, ok := v.(Exporter); ok {
+		x := e.Export()
+		if x.Type().ConvertibleTo(t) {
+			return x.Convert(t), nil
+		}
+	}
+
+	number := func(from apl.Value, to apl.Number) (apl.Number, error) {
+		src, ok := from.(apl.Number)
+		if ok == false {
+			return nil, fmt.Errorf("not a number: %T", from)
+		}
+		_, n, err := a.Tower.SameType(src, to)
+		if err != nil {
+			return nil, err
+		}
+		if reflect.TypeOf(to) != reflect.TypeOf(n) {
+			return nil, fmt.Errorf("cannot convert to %T", to)
+		}
+		return n, nil
+	}
+	zero := reflect.Value{}
+	switch t.Kind() {
+
+	case reflect.Int:
+		if n, err := number(v, numbers.Integer(0)); err != nil {
+			return zero, err
+		} else {
+			return reflect.ValueOf(int(n.(numbers.Integer))), nil
+		}
+
+	case reflect.Float64:
+		if n, err := number(v, numbers.Float(0)); err != nil {
+			return zero, err
+		} else {
+			return reflect.ValueOf(float64(n.(numbers.Float))), nil
+		}
+
+	case reflect.Complex128:
+		if n, err := number(v, numbers.Complex(0)); err != nil {
+			return zero, err
+		} else {
+			return reflect.ValueOf(complex128(n.(numbers.Complex))), nil
+		}
+
+	case reflect.String:
+		if s, ok := v.(apl.String); ok == false {
+			return zero, fmt.Errorf("expected string")
+		} else {
+			return reflect.ValueOf(string(s)), nil
+		}
+
+	case reflect.Slice:
+		ar, ok := v.(apl.Array)
+		if ok == false {
+			return zero, fmt.Errorf("expected slice")
+		}
+		et := t.Elem()
+		//fmt.Println(t, et)
+		n := apl.ArraySize(ar)
+		s := reflect.MakeSlice(t, n, n)
+		for i := 0; i < n; i++ {
+			if v, err := ar.At(i); err != nil {
+				return zero, err
+			} else {
+				if e, err := export(a, v, et); err != nil {
+					return zero, err
+				} else {
+					se := s.Index(i)
+					se.Set(e)
+				}
+			}
+		}
+		return s, nil
+
+	default:
+		return zero, fmt.Errorf("cannot convert to %v", t)
+	}
+}
+
+// convert converts a go value to an apl value.
+func convert(a *apl.Apl, v reflect.Value) (apl.Value, error) {
+
+	if x := a.Import(v); x != nil {
+		return x, nil
+	}
+
+	switch v.Kind() {
+
+	case reflect.Int:
+		return apl.Index(int(v.Int())), nil
+
+	case reflect.Float64:
+		return numbers.Float(v.Float()), nil
+
+	case reflect.Complex128:
+		return numbers.Complex(v.Complex()), nil
+
+	case reflect.String:
+		return apl.String(v.String()), nil
+
+	case reflect.Slice:
+		n := v.Len()
+		ar := apl.GeneralArray{Dims: []int{n}, Values: make([]apl.Value, n)}
+		for i := range ar.Values {
+			if e, err := convert(a, v.Index(i)); err != nil {
+				return nil, err
+			} else {
+				ar.Values[i] = e
+			}
+		}
+		return ar, nil
+
+	default:
+		return nil, fmt.Errorf("cannot convert %s to an apl value", v.Kind())
+	}
+}
---
+++ apl/xgo/function.go
@@ -0,0 +1,97 @@
+package xgo
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/ktye/iv/apl"
+)
+
+type Function struct {
+	Name string
+	Fn   reflect.Value
+}
+
+func (f Function) String(a *apl.Apl) string {
+	return f.Name
+}
+
+// Call a go function.
+// If it requires 1 argument, that is taken from the right value.
+// Two arguments may be the right and left argument or a vector of 2 arguments.
+// More than two arguments must be passed in a vector of the right size.
+func (f Function) Call(a *apl.Apl, L, R apl.Value) (apl.Value, error) {
+	errarg := func(i int, err error) error {
+		return fmt.Errorf("function %s argument %d: %s", f.Name, i+1, err)
+	}
+	t := f.Fn.Type()
+	args := t.NumIn()
+	in := make([]reflect.Value, args)
+	var err error
+	if args == 1 {
+		in[0], err = export(a, R, t.In(0))
+		if err != nil {
+			return nil, errarg(0, err)
+		}
+	} else if args == 2 && L != nil {
+		in[0], err = export(a, R, t.In(0))
+		if err != nil {
+			return nil, errarg(0, err)
+		}
+		in[1], err = export(a, L, t.In(1))
+		if err != nil {
+			return nil, errarg(1, err)
+		}
+	} else if L == nil {
+		ar, ok := R.(apl.Array)
+		if ok == false {
+			return nil, fmt.Errorf("function %s requires %d arguments", f.Name)
+		}
+		rs := ar.Shape()
+		if len(rs) > 1 {
+			return nil, fmt.Errorf("function argument has rank %d", len(rs))
+		}
+		if n := apl.ArraySize(ar); n != args {
+			return nil, fmt.Errorf("function %s requires %d arguments, R has size %d", f.Name, args, n)
+		} else {
+			for i := 0; i < args; i++ {
+				v, err := ar.At(i)
+				if err != nil {
+					return nil, err
+				}
+				in[i], err = export(a, v, t.In(i))
+				if err != nil {
+					return nil, errarg(i, err)
+				}
+			}
+		}
+	}
+	out := f.Fn.Call(in)
+
+	// Test if the last output value is an error, check and remove it.
+	if len(out) > 0 {
+		if last := out[len(out)-1]; last.Type().Implements(reflect.TypeOf((*error)(nil)).Elem()) {
+			if last.IsNil() == false {
+				return nil, last.Interface().(error)
+			} else {
+				out = out[:len(out)-1]
+			}
+		}
+	}
+
+	if len(out) == 0 {
+		return apl.EmptyArray{}, nil
+	} else if len(out) == 1 {
+		return convert(a, out[0])
+	} else {
+		res := apl.GeneralArray{Dims: []int{len(out)}, Values: make([]apl.Value, len(out))}
+		for i := range out {
+			if v, err := convert(a, out[i]); err != nil {
+				return nil, err
+			} else {
+				res.Values[i] = v
+			}
+		}
+		return res, nil
+	}
+}
